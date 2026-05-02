c6d6a94fba6c86f995459e171544bdca46548e72
ahmedyasser1234 <ahmedyasser697@gmail.com>
2026-05-02 04:40:36 +0300

    updated

--- server-nestjs/src/ai-subscriptions/ai-subscriptions.module.ts
+++ server-nestjs/src/ai-subscriptions/ai-subscriptions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AiSubscriptionsService } from './ai-subscriptions.service';
 import { AiSubscriptionsController } from './ai-subscriptions.controller';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [AiSubscriptionsController],
   providers: [AiSubscriptionsService],
   exports: [AiSubscriptionsService],
